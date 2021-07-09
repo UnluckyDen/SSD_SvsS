0b296af19acfca70df9c28c5201bd8c677f73958
UnluckyDen <dsgladkih01@gmail.com>
2021-07-09 12:53:41 +0300

    AddEffectSystem

--- SvsS/Assets/Scripts/Card/Data/CardData.cs
+++ SvsS/Assets/Scripts/Card/Data/CardData.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Experimental.GlobalIllumination;
 
@@ -6,15 +7,12 @@ namespace Card.Data
     [CreateAssetMenu(fileName = "CardData",menuName = "Card Data",order = 0)]
     public class CardData : ScriptableObject
     {
-        [System.Serializable]
-        public struct CardSettings
-        {
-            public string name;
+            public new string name;
             public string description;
             public string manaCost;
             public Sprite sprite;
+            public GameObject cardPrefab;
             
-            
-        }
+            public List<EffectsData> cardEffect;
     }
 }
