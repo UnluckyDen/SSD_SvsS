3a74f88d30ddfecd4955b8cae31e199b45c8dc76
UnluckyDen <dsgladkih01@gmail.com>
2021-07-12 17:42:13 +0300

    Add card creator and card tamplate

---
+++ SvsS/Assets/Scripts/Systems/CardCreator.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Card.Data;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Systems
+{
+    public class CardCreator : MonoBehaviour
+    {
+
+        public void CreateCard(CardData card)
+        {
+            var cartGAmeObject = Instantiate(card.cardPrefab,new Vector3(0f,0f,0f),Quaternion.Euler(-90f,0f,0f));
+            cartGAmeObject.GetComponentInChildren<Image>().sprite = card.sprite;
+            
+            TextMeshProUGUI[] texts = cartGAmeObject.GetComponentsInChildren<TextMeshProUGUI>();
+            
+            texts[0].text = card.name;
+            texts[1].text = card.description;
+            texts[2].text = (card.manaCost).ToString();
+        }
+    }
+}
